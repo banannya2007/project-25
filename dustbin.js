292552f08cb6364c6250cb1e1648ad76f243d579
banannya2007 <73088793+banannya2007@users.noreply.github.com>
2020-12-22 20:30:52 +0800

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,18 @@
+class dustbin 
+{
+constructor (x, y,r)
+{
+    var options= {
+        IsStatic:false, 
+        restitution:0.3, 
+        friction:0.5, 
+        density:1.2
+      }
+      this.image = loadImage("sprites/dustbingreen-1.png")
+} 
+}
+ // this.x=x;
+ // this.y=y;
+ // this.r=r
+ // world.add(world, this.body);
+ // this.body=bodies.circle(this.x, this.y, this.r/2, options)
